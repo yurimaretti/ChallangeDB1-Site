2269375b3f398f8ccb626ad921492e4f4f8e5202
yurimaretti <yurimaretti@hotmail.com>
2024-05-14 18:16:49 -0300

    Inclusao do bootstrap, criação do componente Header e da View de Login, bem como definição desta como rota padrão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
