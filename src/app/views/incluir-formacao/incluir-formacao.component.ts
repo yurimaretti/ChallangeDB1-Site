41233f10a32d0cdb0e48333aa2fbccb473c17183
yurimaretti <yurimaretti@hotmail.com>
2024-05-17 18:34:07 -0300

    Validação do formulário para incluir formação

--- src/app/views/incluir-formacao/incluir-formacao.component.ts
+++ src/app/views/incluir-formacao/incluir-formacao.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { GrauInstrucao } from 'src/app/enums/grauInstrucao';
 
 @Component({
@@ -6,6 +8,27 @@ import { GrauInstrucao } from 'src/app/enums/grauInstrucao';
   templateUrl: './incluir-formacao.component.html',
   styleUrls: ['./incluir-formacao.component.scss']
 })
-export class IncluirFormacaoComponent {
+export class IncluirFormacaoComponent implements OnInit {
+
+  formulario!: FormGroup;
   grauEnum = Object.values(GrauInstrucao);
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      nivel: ['', Validators.required],
+      curso: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(70)]],
+      instituicao: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(70)]]
+    });
+  }
+
+  cadastrarFormacao() {
+    if (this.formulario.valid) {
+      this.router.navigate(['/editar-formacao'])
+    }
+  }
 }
