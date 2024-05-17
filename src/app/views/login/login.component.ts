2715d5f1d7f3fa4e305e9012ce234267f2d190d9
yurimaretti <yurimaretti@hotmail.com>
2024-05-17 18:12:10 -0300

    Validação do formulário Login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  formulario!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      emailUsuario: ['', [Validators.required, Validators.email]],
+      tipoCadastro: ['', Validators.required],
+      senhaUsuario: ['', Validators.required]
+    });
+  }
+
+  realizarLogin() {
+    if (this.formulario.valid) {
+      this.router.navigate(['/inicio'])
+    }
+  }
 }
