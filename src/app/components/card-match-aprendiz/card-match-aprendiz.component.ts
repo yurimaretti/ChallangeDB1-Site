e0043bea31d099706f05f82c2e9393f038ca259e
yurimaretti <yurimaretti@hotmail.com>
2024-05-17 14:15:23 -0300

    Finalização dos cards match para mentores e aprendizes e da tela de Matches

--- src/app/components/card-match-aprendiz/card-match-aprendiz.component.ts
+++ src/app/components/card-match-aprendiz/card-match-aprendiz.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-match-aprendiz',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-match-aprendiz.component.scss']
 })
 export class CardMatchAprendizComponent {
-
+  @Input() nomeAprendiz: String = "Yuri Maretti Cornacioni";
+  @Input() emailAprendiz: String = "yurimaretti@hotmail.com";
+  @Input() generoAprendiz: String = "Masculino";
+  @Input() interesseAprendiz: String = "História, Tecnologia";
+  @Input() nivelFormacao: String = "Superior - Tecnólogo";
+  @Input() nomeCurso: String = "Análise e Desenvolvimento de Sistemas";
+  @Input() instituicao: String = "FIAP";
 }
--- src/app/components/card-match-mentor/card-match-mentor.component.ts
+++ src/app/components/card-match-mentor/card-match-mentor.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-match-mentor',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-match-mentor.component.scss']
 })
 export class CardMatchMentorComponent {
-
+  @Input() nomeMentor: String = "Duílio Alencar";
+  @Input() emailMentor: String = "duds@yahoo.com";
+  @Input() generoMentor: String = "Masculino";
+  @Input() interesseMentor: String = "Direito, Esportes";
+  @Input() nivelFormacao: String = "Superior - Bacharel";
+  @Input() nomeCurso: String = "Direito";
+  @Input() instituicao: String = "Universidade ABC";
 }
