6a57baa7c38a79d04c3921661ce2b4a04380e26f
yurimaretti <yurimaretti@hotmail.com>
2024-05-18 20:25:43 -0300

    Funcionalidade de curtir card do Aprendiz/Mentor implantada

--- src/app/components/card-aprendiz/card-aprendiz.component.ts
+++ src/app/components/card-aprendiz/card-aprendiz.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { InteresseModel } from 'src/app/models/interesseModel';
+import { ApiService } from 'src/app/services/api-service.service';
+import { MatchModel } from 'src/app/models/matchModel';
+import { EMPTY, catchError, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-card-aprendiz',
@@ -7,7 +10,64 @@ import { InteresseModel } from 'src/app/models/interesseModel';
   styleUrls: ['./card-aprendiz.component.scss']
 })
 export class CardAprendizComponent {
+
   @Input() nomeAprendiz: String = "";
   @Input() generoAprendiz: String = "";
   @Input() interesseAprendiz: InteresseModel[] | undefined;
+  @Input() emailMentor: string = '';
+  @Input() emailAprendiz: string = '';
+
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  curtirAprendiz(): void {
+
+    this.apiService.getMatchPorEmails(this.emailAprendiz, this.emailMentor).pipe(
+      switchMap((match: MatchModel) => {
+        if (match) {
+          match.curtidaMentor = 1; // Alterando a curtidaMentor para 1
+          return this.apiService.atualizarMatch(match.matchId, match);
+        }
+        catchError((error) => {
+          console.error('Erro ao curtir aprendiz:', error);
+          return EMPTY; // Retorna um Observable vazio para evitar erros
+        })
+        const novoMatch: MatchModel = {
+          matchId: 0,
+          curtidaAprendiz: 0,
+          curtidaMentor: 1,
+          emailAprdz: this.emailAprendiz,
+          emailMentor: this.emailMentor
+        };
+        return this.apiService.incluirMatch(novoMatch).pipe(
+          catchError((error) => {
+            alert('Erro ao incluir novo match');
+            return of(null); // Retorna um Observable nulo para evitar erros
+          })
+        );
+      }),
+      catchError((error) => {
+        // Não existe um match, incluir novo match
+        const novoMatch: MatchModel = {
+          matchId: 0,
+          curtidaAprendiz: 0,
+          curtidaMentor: 1,
+          emailAprdz: this.emailAprendiz,
+          emailMentor: this.emailMentor
+        };
+        this.apiService.incluirMatch(novoMatch).subscribe(incluido => {
+          alert('Aprendiz Curtido!');
+        });
+        return EMPTY; // Retorna um Observable vazio para evitar erros
+      })
+    ).subscribe((resposta) => {
+      if (resposta) {
+        alert('Aprendiz Curtido!');
+      } else {
+        alert('Aprendiz Curtido!');
+      }
+    });
+  }
 }
+
--- src/app/components/card-mentor/card-mentor.component.ts
+++ src/app/components/card-mentor/card-mentor.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { HabilidadeModel } from 'src/app/models/habilidadeModel';
+import { ApiService } from 'src/app/services/api-service.service';
+import { MatchModel } from 'src/app/models/matchModel';
+import { EMPTY, catchError, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-card-mentor',
@@ -7,7 +10,63 @@ import { HabilidadeModel } from 'src/app/models/habilidadeModel';
   styleUrls: ['./card-mentor.component.scss']
 })
 export class CardMentorComponent {
+
   @Input() nomeMentor: String = "";
   @Input() generoMentor: String = "";
   @Input() habilidadeMentor: HabilidadeModel[] | undefined;
+  @Input() emailMentor: string = '';
+  @Input() emailAprendiz: string = '';
+
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  curtirMentor(): void {
+
+    this.apiService.getMatchPorEmails(this.emailAprendiz, this.emailMentor).pipe(
+      switchMap((match: MatchModel) => {
+        if (match) {
+          match.curtidaAprendiz = 1; // Alterando a curtidaAprendiz para 1
+          return this.apiService.atualizarMatch(match.matchId, match);
+        }
+        catchError((error) => {
+          console.error('Erro ao curtir aprendiz:', error);
+          return EMPTY; // Retorna um Observable vazio para evitar erros
+        })
+        const novoMatch: MatchModel = {
+          matchId: 0,
+          curtidaAprendiz: 1,
+          curtidaMentor: 0,
+          emailAprdz: this.emailAprendiz,
+          emailMentor: this.emailMentor
+        };
+        return this.apiService.incluirMatch(novoMatch).pipe(
+          catchError((error) => {
+            alert('Erro ao incluir novo match');
+            return of(null); // Retorna um Observable nulo para evitar erros
+          })
+        );
+      }),
+      catchError((error) => {
+        // Não existe um match, incluir novo match
+        const novoMatch: MatchModel = {
+          matchId: 0,
+          curtidaAprendiz: 1,
+          curtidaMentor: 0,
+          emailAprdz: this.emailAprendiz,
+          emailMentor: this.emailMentor
+        };
+        this.apiService.incluirMatch(novoMatch).subscribe(incluido => {
+          alert('Mentor Curtido!');
+        });
+        return EMPTY; // Retorna um Observable vazio para evitar erros
+      })
+    ).subscribe((resposta) => {
+      if (resposta) {
+        alert('Mentor Curtido!');
+      } else {
+        alert('Mentor Curtido!');
+      }
+    });
+  }
 }
