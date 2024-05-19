32c88466fb4538c6077c4ca94756289d8f796b40
yurimaretti <yurimaretti@hotmail.com>
2024-05-18 21:15:42 -0300

    Funcionalidade Desfazer Match implantada

--- src/app/components/card-match-aprendiz/card-match-aprendiz.component.ts
+++ src/app/components/card-match-aprendiz/card-match-aprendiz.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { FormAprdzModel } from 'src/app/models/formAprendizModel';
 import { InteresseModel } from 'src/app/models/interesseModel';
+import { MatchModel } from 'src/app/models/matchModel';
+import { ApiService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-card-match-aprendiz',
@@ -8,9 +10,27 @@ import { InteresseModel } from 'src/app/models/interesseModel';
   styleUrls: ['./card-match-aprendiz.component.scss']
 })
 export class CardMatchAprendizComponent {
+
+  @Input() emailMentor: string = "";
   @Input() nomeAprendiz: String = "";
-  @Input() emailAprendiz: String = "";
+  @Input() emailAprendiz: string = "";
   @Input() generoAprendiz: String = "";
   @Input() interesseAprendiz: InteresseModel[] | undefined;
   @Input() formacaoAprendiz: FormAprdzModel[] | undefined;
+
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  desfazerMatch(): void {
+    this.apiService.getMatchPorEmails(this.emailAprendiz, this.emailMentor).subscribe(match => {
+      if (match) {
+        const updatedMatch: MatchModel = { ...match, curtidaMentor: 0 };
+        this.apiService.atualizarMatch(match.matchId, updatedMatch).subscribe(updated => {
+          alert('Match desfeito!');
+          window.location.reload()
+        });
+      }
+    });
+  }
 }
--- src/app/components/card-match-mentor/card-match-mentor.component.ts
+++ src/app/components/card-match-mentor/card-match-mentor.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { FormMentorModel } from 'src/app/models/formMentorModel';
 import { HabilidadeModel } from 'src/app/models/habilidadeModel';
+import { MatchModel } from 'src/app/models/matchModel';
+import { ApiService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-card-match-mentor',
@@ -8,9 +10,27 @@ import { HabilidadeModel } from 'src/app/models/habilidadeModel';
   styleUrls: ['./card-match-mentor.component.scss']
 })
 export class CardMatchMentorComponent {
+
+  @Input() emailAprendiz: string = "";
   @Input() nomeMentor: String = "";
-  @Input() emailMentor: String = "";
+  @Input() emailMentor: string = "";
   @Input() generoMentor: String = "";
   @Input() habilidadeMentor: HabilidadeModel[] | undefined;
   @Input() formacaoMentor: FormMentorModel[] | undefined;
+
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  desfazerMatch(): void {
+    this.apiService.getMatchPorEmails(this.emailAprendiz, this.emailMentor).subscribe(match => {
+      if (match) {
+        const updatedMatch: MatchModel = { ...match, curtidaAprendiz: 0 };
+        this.apiService.atualizarMatch(match.matchId, updatedMatch).subscribe(updated => {
+          alert('Match desfeito!');
+          window.location.reload()
+        });
+      }
+    });
+  }
 }
