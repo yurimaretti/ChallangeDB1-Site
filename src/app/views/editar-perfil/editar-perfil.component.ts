d234b90a5143081ed1b9a632c9ff893a0f5a3fcd
yurimaretti <yurimaretti@hotmail.com>
2024-05-18 22:13:29 -0300

    Consulta aos dados do usuário implantada

--- src/app/views/editar-perfil/editar-perfil.component.ts
+++ src/app/views/editar-perfil/editar-perfil.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Genero } from 'src/app/enums/generos';
+import { AprendizModel } from 'src/app/models/aprendizModel';
+import { HabilidadeModel } from 'src/app/models/habilidadeModel';
+import { InteresseModel } from 'src/app/models/interesseModel';
+import { MentorModel } from 'src/app/models/mentorModel';
+import { ApiService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-editar-perfil',
@@ -14,11 +19,16 @@ export class EditarPerfilComponent implements OnInit {
   generosEnum = Object.values(Genero);
   tipoCadastro: string = '';
   email: string = '';
+  interessesAprendiz: InteresseModel[] | undefined;
+  habilidadesMentor: HabilidadeModel[] | undefined;
+  areaInteresse: string = '';
+  areaHabilidade: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private apiService: ApiService
   ) { }
 
   ngOnInit(): void {
@@ -31,7 +41,35 @@ export class EditarPerfilComponent implements OnInit {
     });
   }
 
-  atualizarCadastro() {
+  consultarDados(): void {
+    if (this.tipoCadastro === 'Aprendiz') {
+      this.apiService.getAprendizPorEmail(this.email).subscribe(
+        (data: AprendizModel) => {
+          this.formulario = this.formBuilder.group({
+            nomeUsuario: [data.nomeAprendiz, [Validators.required, Validators.minLength(2), Validators.maxLength(70)]],
+            genero: [data.generoAprendiz, Validators.required]
+          });
+          data.interesse!.forEach(interesse => {
+            this.areaInteresse = interesse.areaInteresse;
+          });
+        }
+      );
+    } else if (this.tipoCadastro === 'Mentor') {
+      this.apiService.getMentorPorEmail(this.email).subscribe(
+        (data: MentorModel) => {
+          this.formulario = this.formBuilder.group({
+            nomeUsuario: [data.nomeMentor, [Validators.required, Validators.minLength(2), Validators.maxLength(70)]],
+            genero: [data.generoMentor, Validators.required]
+          });
+          data.habilidade!.forEach(habilidade => {
+            this.areaHabilidade = habilidade.areaHabilidade;
+          });
+        }
+      );
+    }
+  }
+
+  atualizarCadastro(): void {
     if (this.formulario.valid) {
       if (this.tipoCadastro === 'Aprendiz') {
         this.router.navigate(['/inicio', this.tipoCadastro, this.email]);
