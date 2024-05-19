661422754b3a5b884735eb4fe56b3977fa611012
yurimaretti <yurimaretti@hotmail.com>
2024-05-18 23:36:40 -0300

    Funcionalidade excluir formação implantada

--- src/app/components/card-form-mentor/card-form-mentor.component.ts
+++ src/app/components/card-form-mentor/card-form-mentor.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ApiService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-card-form-mentor',
@@ -6,7 +7,28 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card-form-mentor.component.scss']
 })
 export class CardFormMentorComponent {
+
   @Input() nomeInstituicao: String = "";
   @Input() nivelEnsino: String = "";
   @Input() nomeCurso: String = "";
+  @Input() idFormacao: number = 0;
+
+  constructor(private apiService: ApiService) {}
+
+  excluirFormacao(): void {
+    if (confirm('Tem certeza que deseja excluir esta formação?')) {
+      this.apiService.excluirFormMentor(this.idFormacao).subscribe(
+        () => {
+          // Sucesso na exclusão
+          alert('Formação excluída com sucesso!');
+          window.location.reload()
+        },
+        error => {
+          // Tratar erro
+          console.error('Erro ao excluir formação:', error);
+          alert('Ocorreu um erro ao excluir a formação. Tente novamente mais tarde.');
+        }
+      );
+    }
+  }
 }
